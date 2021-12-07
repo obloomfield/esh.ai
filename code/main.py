9af913739f5110620de83593f3acef1cc10928be
anonymous0928565 <andrew_truss@brown.edu>
2021-12-07 17:12:12 -0500

    main stensil

---
+++ code/main.py
@@ -0,0 +1,10 @@
+def save_video():
+    # TODO: Take all the progress steps and compile into mp4
+    pass
+
+def main():
+    # TODO: Uhhh do stuff
+    pass
+
+if __name__ == '__main__':
+    main()
