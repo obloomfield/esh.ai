2e00ce1969cf64d3f781197fc76555a2c6b4493f
anonymous0928565 <andrew_truss@brown.edu>
2021-12-07 18:15:04 -0500

    main stensil

--- code/main.py
+++ code/main.py
@@ -2,8 +2,15 @@ def save_video():
     # TODO: Take all the progress steps and compile into mp4
     pass
 
+def train():
+    pass
+
 def main():
-    # TODO: Uhhh do stuff
+    # TODO: Load CLIP
+    # TODO: Load data for VQGAN
+    # TODO: Train VQGAN
+    # TODO: Train to iteratively improve the image
+    # TODO: Compile into mp4
     pass
 
 if __name__ == '__main__':
