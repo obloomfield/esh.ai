0ca39bb41f36038c1bd39c8b6c91fa4c8ddf4337
anonymous0928565 <andrew_truss@brown.edu>
2021-12-08 20:30:59 -0500

    attention

---
+++ code/model/attention.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+class SpatialAttention(tf.keras.Model):
+    def __init__(self):
+        pass
+
+    def call(self):
+        pass
+
+class ChannelAttention(tf.keras.Model):
+    def __init__(self):
+        pass
+
+    def call(self):
+        pass
