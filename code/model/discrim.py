38684b035661ba64112fdc8c059f38bd423eb024
anonymous0928565 <andrew_truss@brown.edu>
2021-12-09 17:10:25 -0500

    cmon

--- code/model/discrim.py
+++ code/model/discrim.py
@@ -13,7 +13,7 @@ class Discriminator(tf.keras.Model):
         
         self.embed = Sequential()
         
-        self.embed.add(Embedding(49, self.embed_size))
+        self.embed.add(Embedding(59, self.embed_size))
         self.embed.add(LeakyReLU(alpha=0.03))
         
         self.D = Sequential()
